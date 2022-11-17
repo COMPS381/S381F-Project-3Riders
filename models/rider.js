808407cd036520533835cbf2cebf8fe7cc41927d
timescam <timescam@duck.com>
2022-11-17 15:16:27 +0800

    makes more sense

--- models/rider.js
+++ models/rider.js
@@ -2,11 +2,11 @@ var mongoose = require("mongoose");
 
 var riderSchema = new mongoose.Schema({
 	sid: { type: String, unique: true, required: true, min: 8, max: 8 },
-	year: { type: Number, max: 1 },
 	name: { type: String },
 	reports: [
 		{
 			username: String,
+			courseCode: { type: String, default: "" },
 			reportDate: { type: Date, default: Date.now },
 			remarks: { type: String, default: "" },
 		},
